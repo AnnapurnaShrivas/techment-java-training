47fee9001d09f564adb7abe88a24abb25b3c4f1d
AnnapurnaShrivas <87543309+AnnapurnaShrivas@users.noreply.github.com>
2021-08-13 20:41:39 +0530

    initial

---
+++ deposite/A.java
@@ -0,0 +1,24 @@
+package com.techment.deposite;
+
+public class A {
+
+	private int a=10;
+	protected int c=20;
+	public int d=30;
+	        int b=40;     // default access modifier
+	        
+	public static void main(String[] args) {
+
+
+		
+		        
+		        A obj =new A();
+		        
+		        System.out.println(obj.a);
+		        System.out.println(obj.b);
+		        System.out.println(obj.c);
+		        System.out.println(obj.d);
+
+	}
+
+}
---
+++ deposite/B.java
@@ -0,0 +1,16 @@
+package com.techment.deposite;
+
+public class B {
+
+	public static void main(String[] args) {
+
+        
+        A obj =new A();
+        
+        //  System.out.println(obj.a);    //a is a private data member.. we can not access it out side the class
+        System.out.println(obj.b);
+        System.out.println(obj.c);
+        System.out.println(obj.d);
+	}
+
+}
